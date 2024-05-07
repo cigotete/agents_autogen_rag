e34a68c5b9817a30eded91af6c5c6e7ccb0454bf
Camilo Figueroa <cigotete@gmail.com>
2024-05-07 12:40:40 -0500

    Adding Agents.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import os
 import random
+import chromadb
+from typing_extensions import Annotated
+
 import autogen
+from autogen import AssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+
 
 config_list = autogen.config_list_from_json(
   "OAI_CONFIG_LIST.json",
@@ -14,3 +21,57 @@ llm_config = {
   "seed": random.randint(100, 100000),
   "config_list": config_list
 }
+
+def termination_msg(x):
+    return isinstance(x, dict) and "TERMINATE" == str(x.get("content", ""))[-9:].upper()
+
+boss = autogen.UserProxyAgent(
+    name="Boss",
+    is_termination_msg=termination_msg,
+    human_input_mode="NEVER",
+    code_execution_config=False,  # we don't want to execute code in this case.
+    default_auto_reply="Reply `TERMINATE` if the task is done.",
+    description="The boss who ask questions and give tasks.",
+)
+
+boss_aid = RetrieveUserProxyAgent(
+    name="Boss_Assistant",
+    is_termination_msg=termination_msg,
+    human_input_mode="NEVER",
+    default_auto_reply="Reply `TERMINATE` if the task is done.",
+    max_consecutive_auto_reply=3,
+    retrieve_config={
+        "task": "code",
+        "docs_path": "https://www.ietf.org/rfc/rfc768.txt",
+        "chunk_token_size": 1000,
+        "model": config_list[0]["model"],
+        "collection_name": "groupchat",
+        "get_or_create": True,
+    },
+    code_execution_config=False,  # we don't want to execute code in this case.
+    description="Assistant who has extra content retrieval power for solving difficult problems.",
+)
+
+coder = AssistantAgent(
+    name="Senior_Software_Engineer",
+    is_termination_msg=termination_msg,
+    system_message="You are a senior software engineer, and you answer questions related. Reply `TERMINATE` in the end when everything is done.",
+    llm_config=llm_config,
+    description="Senior software Engineer who can write code to solve problems and answer questions.",
+)
+
+pm = autogen.AssistantAgent(
+    name="Product_Manager",
+    is_termination_msg=termination_msg,
+    system_message="You are a product manager. Reply `TERMINATE` in the end when everything is done.",
+    llm_config=llm_config,
+    description="Product Manager who can design and plan the project.",
+)
+
+reviewer = autogen.AssistantAgent(
+    name="Code_Reviewer",
+    is_termination_msg=termination_msg,
+    system_message="You are a code reviewer. Reply `TERMINATE` in the end when everything is done.",
+    llm_config=llm_config,
+    description="Code Reviewer who can review the code.",
+)
